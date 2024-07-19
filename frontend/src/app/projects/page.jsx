929d1f2980464c58d18df3354335d43121d6007c
User Taylor <elexus0201@gmail.com>
2024-07-19 19:59:11 -0400

    updating

--- frontend/src/app/projects/page.jsx
+++ frontend/src/app/projects/page.jsx
@@ -1,5 +1,7 @@
 
-import React from 'react'
+// To inform next js, this is a client component 
+"use client"; 
+
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
 
@@ -7,125 +9,75 @@ const Projects = () => {
 
   const [projects, setProjects] = useState([]);
 
-  
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
 
-  return (
+        const response = await fetch("http://localhost:4000/projects", {
+          credentials: 'include'
+        });
+
+        const data = await response.json()
+        setProjects(data)
+
+      } catch (error) {
+        console.error('Error: ', error.message);
+      }
+    };
+
+    fetchProjects()
+  }, []);
+
+  return(
+
     <div>
 
-      <div className="flex flex-col items-center text-center p-4">
-        <h2 className="text-4xl font-bold text-black mb-4">My Projects</h2>
-        <p className="text-lg text-black">
-          Visit my <Link href="https://github.com/Taylor-99" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700 visited:text-purple-600">GitHub</Link>
-        </p>
-      </div>
+        <div className="flex flex-col items-center text-center p-4">
+          <h2 className="text-4xl font-bold text-black mb-4">My Projects</h2>
+          <p className="text-lg text-black">
+            Visit my <Link href="https://github.com/Taylor-99" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700 visited:text-purple-600">GitHub</Link>
+          </p>
+        </div>
 
       <br></br>
 
-      <div className="bg-white bg-opacity-40 rounded-lg shadow-md p-4" >
+      <ul className="grid grid-rows sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          { projects && projects.map((project, idx) => {
 
-        <h1 className="text-2xl text-center text-black">Pirate Adventure Trivia Game</h1>
+            const loaded = () => {
 
-          <br></br>
+              return(
 
-        <Link href="https://pirate-adventure-trivia-game.vercel.app" target="_blank" rel="noopener noreferrer">
-          <img src='https://imgur.com/cGs17gy.png' className="block align-middle w-auto" alt="Project Cover"/>
-        </Link>
+                <li key={idx} className="bg-white bg-opacity-40 rounded-lg shadow-md p-4 w-45">
+                  <h1 className="text-2xl text-center text-black overflow-hidden text-ellipsis whitespace-nowrap">{project.name}</h1>
 
-          <br></br>
+                  <br></br>
 
-        <div className="flex justify-center">
-          <Link href="https://github.com/Taylor-99/Project-1-Trivia-Game" className="bg-pink-400 text-2xl text-white px-8 py-4 hover:bg-pink-500 rounded-md" target="_blank" rel="noopener noreferrer">
-              Github
-          </Link>
-        </div>
+                  <Link href={project.live} target="_blank" rel="noopener noreferrer">
+                    <img src={project.image} className="block align-middle w-auto" alt="Project Cover"/>
+                  </Link>
 
-      </div>
+                  <br></br>
 
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
+                  <div className="flex justify-center">
+                    <Link href={project.git} className="bg-pink-400 text-2xl text-white px-8 py-4 hover:bg-pink-500 rounded-md" target="_blank" rel="noopener noreferrer">
+                        Github
+                    </Link>
+                  </div>
 
-      {/* <div className="bg-white bg-opacity-40 rounded-lg shadow-md p-4" >
+                </li>
 
-        <h1 className="text-2xl text-center text-black">Reader's Jounal</h1>
+              );
+            }
 
-          <br></br>
+              return projects ? loaded() : <h1>Loading...</h1>;
 
-        <Link href="https://readersjournal-27f55d6eb9b9.herokuapp.com/library">
-          <img src='https://imgur.com/nCeGYzj.png' className="block align-middle w-auto"/>
-        </Link>
+          })};
 
-          <br></br>
-
-        <div className="flex justify-center">
-          <Link href="https://github.com/Taylor-99/Readers-Journal"
-            className="bg-pink-400 text-2xl text-white px-8 py-4 hover:bg-pink-500 rounded-md">
-              Github
-          </Link>
-        </div>
-
-      </div>
-
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-
-      <div className="bg-white bg-opacity-40 rounded-lg shadow-md p-4" >
-
-        <h1 className="text-2xl text-center text-black">Sound Scope</h1>
-
-          <br></br>
-
-        <Link href="https://soundscope.netlify.app/">
-          <img src='https://imgur.com/wloR6Gr.png' className="block align-middle w-auto"/>
-        </Link>
-
-          <br></br>
-
-        <div className="flex justify-center">
-          <Link href="https://github.com/GrabberKaibutsu/SoundScope"
-            className="bg-pink-400 text-2xl text-white px-8 py-4 hover:bg-pink-500 rounded-md">
-              Github
-          </Link>
-        </div>
-
-      </div>
-
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-
-      <div className="bg-white bg-opacity-40 rounded-lg shadow-md p-4" >
-
-        <h1 className="text-2xl text-center text-black">Garden Health</h1>
-
-          <br></br>
-
-        <Link href="https://garden-health-frontend-ok73.vercel.app/auth">
-          <img src='https://imgur.com/5DC6Wdb.png' className="block align-middle w-auto"/>
-        </Link>
-
-          <br></br>
-
-        <div className="flex justify-center">
-          <Link href="https://github.com/Taylor-99/Garden-Health-Frontend"
-            className="bg-pink-400 text-2xl text-white px-8 py-4 hover:bg-pink-500 rounded-md">
-              Github
-          </Link>
-        </div>
-
-      </div>
-
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br> */}
-
+      </ul>
     </div>
-  )
-}
+
+    )
+  }
 
 export default Projects
